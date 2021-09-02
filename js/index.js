8c22907b488ec006c95577c7150b0e27f5d4b765
showkhin1997 <showkhin15@gmail.com>
2021-09-02 14:44:21 +0600

    search result error fix

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
     searchField.value = '';
+
+    // load data 
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
@@ -12,6 +13,14 @@ const searchBook = () => {
 
 const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if (books.length === 0) {
+        searchResult.innerHTML = `
+        <div class="text-center text-danger fs-1">
+          <h1>!!Search Result Not Found!!</h1>
+        </div>
+        `;
+    }
     books.forEach(book => {
         const imageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         const div = document.createElement('div');
